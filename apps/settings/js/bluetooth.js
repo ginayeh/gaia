783a5b3a15d1ea37a10903537aee1c5d111aeaf4
Gina Yeh <gyeh@mozilla.com>
2012-08-14 13:30:36 +0800

    merge with latest version of gaia

--- apps/settings/js/bluetooth.js
+++ apps/settings/js/bluetooth.js
@@ -6,8 +6,10 @@
 // create a fake mozBluetooth if required (e.g. desktop browser)
 var gBluetooth = (function(window) {
   var navigator = window.navigator;
-  if (('mozBluetooth' in navigator) && navigator.mozBluetooth)
+  if (('mozBluetooth' in navigator) && navigator.mozBluetooth) {
+    dump("gBluetooth = navigator.mozBluetooth");
     return navigator.mozBluetooth;
+  }
 
   var enabled = false;
   return {
@@ -30,17 +32,114 @@ window.addEventListener('localized', function bluetoothSettings(evt) {
   var settings = window.navigator.mozSettings;
   var gBluetoothInfoBlock = document.querySelector('#bluetooth-status small');
   var gBluetoothCheckBox = document.querySelector('#bluetooth-status input');
+  var gBluetoothPairedInfoBlock = document.querySelector('#bluetooth-paired small');
+  var gBluetoothPairedCheckBox = document.querySelector('#bluetooth-paired input');
+
+  var gBluetoothDefaultAdapter = null;
+  var gPairedDevices = null;
 
   // display Bluetooth power state
   function updatePowerState(value) {
     gBluetoothInfoBlock.textContent = value ? _('enabled') : _('disabled');
     gBluetoothCheckBox.checked = value;
+    if (value) {
+			dump("update Power State to enabled");
+	    window.setTimeout(getDefaultAdapter, 1000);
+    }
   }
 
   // activate main button
   gBluetoothCheckBox.onchange = function changeBT() {
     if (settings) {
       settings.getLock().set({'bluetooth.enabled': this.checked});
+			dump("changeBT()");
+    }
+  };
+
+  gBluetoothPairedCheckBox.onchange = function changePairedBT() {
+		dump("changePairedBT()");
+/*    if (!gBluetooth.enabled) {
+      dump("Bluetooth has not enabled.");
+      return;
+    }*/
+
+		if (!gBluetoothDefaultAdapter) {
+			dump("BluetoothAdapter is null.");
+			return;
+		}
+		
+		dump("call getPairedDevices()");
+    var req = gBluetoothDefaultAdapter.getPairedDevices();
+    
+    req.onsuccess = function bt_getPairedDevicesSuccess() {
+			dump("getPairedDevice Success");
+			gPairedDevices = req.result;
+			dump(gPairedDevices.length);
+			for (var i = 0; i < gPairedDevices.length; i++) {
+				for (var property in gPairedDevices[i]) {
+					dump(gPairedDevices[i][property]);
+				}
+			}
+    };
+
+    req.onerror = function bt_getPairedDevicesError() {
+      dump("Error on get paired-devices");
+    }
+  };
+
+
+  function getDefaultAdapter() {
+    var req = gBluetooth.getDefaultAdapter();
+    dump("call getDefaultAdapter()");
+    req.onsuccess = function bt_getDefaultAdapterSuccess() {
+      gBluetoothDefaultAdapter = req.result;
+
+			dump("get Default Adapter Success");
+
+      gBluetoothDefaultAdapter.onrequestconfirmation = function(evt) {
+        dump("[Gaia] Passkey = " + evt.passkey + ", request confirmation");
+        dump("[Gaia] Device path = " + evt.deviceAddress);
+
+        gBluetoothDefaultAdapter.setPairingConfirmation(evt.deviceAddress, true)
+        NotificationHelper.send("Bluetooth", "test", null);
+      };
+      
+      gBluetoothDefaultAdapter.onrequestpincode = function(evt) {
+        dump("[Gaia] We need to send a set of pin code back!");
+        gBluetoothDefaultAdapter.setPinCode(evt.deviceAddress, "0000");
+      };
+
+      gBluetoothDefaultAdapter.onrequestpasskey = function(evt) {
+        dump("[Gaia] We need to send a set of passkey back!");
+      };
+
+      gBluetoothDefaultAdapter.oncancel = function(evt) {
+        dump("[Gaia] Cancel");
+      };
+
+      gBluetoothDefaultAdapter.ondevicefound = function(evt) {
+        dump("[Gaia] Device found!!!");
+        var i;
+        var len = gDiscoveredDevices.length;
+
+        for (i = 0;i < len;++i) {
+          if (gDiscoveredDevices[i].address == evt.device.address) {
+            break;
+          }
+        }
+
+        if (i == len) {
+          if (evt.device.name != "") {
+            gDeviceList.appendChild(newScanItem(i, evt.device.name));
+            gDiscoveredDevices[i] = evt.device;
+          }
+        }
+      };
+    };
+
+    req.onerror = function bt_getDefaultAdapterError() {
+      dump("ADAPTER GET ERROR");
+      dump(req.error.name);
     }
   };
 
